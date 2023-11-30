ce8eeeeaab2765411bc2a0512ce609737ada13e0
RondaKuraI <johnronda0527@gmail.com>
2023-12-01 00:00:03 +0800

    added Login validation

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,4 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->match(['post', 'get'], '/api/login', 'UserController::login');
---
+++ app/Controllers/UserController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Controllers;
+use CodeIgniter\RESTful\ResourceController;
+use CodeIgniter\API\ResponseTrait;
+
+use App\Models\UserModel;
+use App\Controllers\BaseController;
+
+class UserController extends ResourceController
+{
+    //REGISTER
+    // public function register(){
+    //     // Assuming you have received the email and password from the registration form
+    //     $email = $this->request->getVar("email");
+    //     $password = $this->request->getVar("password");
+    
+    //     // Hash the password
+    //     $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+    
+    //     $user = new UserModel();
+        
+    //     // Save the hashed password in the database
+    //     $user->insert([
+    //         'email' => $email,
+    //         'password' => $hashedPassword,
+    //         // other fields...
+    //     ]);
+    
+    //     // You might want to add some success handling here
+    // }
+    
+    public function login(){
+        $email = $this->request->getVar("email");
+        $password = $this->request->getVar("password");
+        $user = new UserModel();
+        $data = $user->where('email', $email)->first();
+        if ($data) {
+            $pass = $data['password'];
+            $authenticatedPassword = password_verify($password, $pass);
+            if($authenticatedPassword){
+                return $this->respond(['msg' => 'okay', 'token' => $data['token']], 200);
+            }
+            else{
+                return $this->respond(['msg' => 'Invalid Password'], 200);
+            }
+        }
+        else{
+            return $this->respond(['msg' => 'No User Found']);
+        }
+    }
+    
+
+    public function index()
+    {
+        //
+    }
+}
